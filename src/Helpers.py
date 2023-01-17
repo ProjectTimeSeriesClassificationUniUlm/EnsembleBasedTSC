0b72c7775e5721d7b8ca3e51fd3df8e46b707305
Tim Wibiral <tim.wibiral.tw@gmail.com>
2023-01-17 15:07:30 +0100

    fix: storing results in csv

--- src/Helpers.py
+++ src/Helpers.py
@@ -2,7 +2,7 @@ from csv import DictWriter
 import csv
 
 def append_to_csv(csv_path, input_row):
-    with open(csv_path, 'a', newline='') as f_object:
+    with open(csv_path, 'a') as f_object:
         writer = csv.writer(f_object)
         writer.writerow(input_row)
 
