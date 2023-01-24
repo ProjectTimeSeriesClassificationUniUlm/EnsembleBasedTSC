bde170454ab9df9e5ce06d13165258dc3ea69b4d
dgb62@uni-ulm.de <ul_dgb62@uc2n368.localdomain>
2023-01-24 11:58:44 +0100

    create images of confusion matrices

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -3,32 +3,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def plot_model_history(history, epochs):
+def plot_model_history(history, epochs, path_to_persist=None):
     loss = history['loss']
     val_loss = history['val_loss']
 
     accuracy = history['accuracy']
     val_accuracy = history['val_accuracy']
-    print(f'Highest Validation Accuracy: {np.max(val_accuracy)}')
     eps = range(epochs)
 
-    plt.figure()
-    plt.plot(eps, loss, 'r', label='Training loss')
-    plt.plot(eps, val_loss, 'b', label='Validation loss')
-    plt.title('Training and Validation Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss Value')
-    plt.ylim([0, 2])
-    plt.legend()
-    plt.show()
+    figure, axis = plt.subplots(2, 1)
+    axis[0].plot(eps, loss, 'r', label='Training loss')
+    axis[0].plot(eps, val_loss, 'b', label='Validation loss')
+    axis[0].set_title('Training and Validation Loss')
+    axis[0].set_xlabel('Epoch')
+    axis[0].set_ylabel('Loss Value')
+    axis[0].set_ylim([0, 2])
+    axis[0].legend()
 
-    plt.figure()
-    plt.plot(eps, accuracy, 'r', label='Training accuracy')
-    plt.plot(eps, val_accuracy, 'b', label='Validation accuracy')
-    plt.title('Training and Validation accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy Value')
-    plt.ylim([0, 1])
-    plt.legend()
-    plt.show()
+    axis[1].plot(eps, accuracy, 'r', label='Training accuracy')
+    axis[1].plot(eps, val_accuracy, 'b', label='Validation accuracy')
+    axis[1].set_title('Training and Validation accuracy')
+    axis[1].set_xlabel('Epoch')
+    axis[1].set_ylabel('Accuracy Value')
+    axis[1].set_ylim([0, 1])
+    axis[1].legend()
+    
+    figure.tight_layout()
+    if path_to_persist:
+        figure.savefig(path_to_persist)
+        plt.close(figure)
+    else:
+        print(f'Highest Validation Accuracy: {np.max(val_accuracy)}')
+        plt.show()
 
