6f4652ffb7c6a19683b0fabc1bf87ea6f76e1ced
Felix Burr <felix.burr@uni-ulm.de>
2023-01-30 21:12:27 +0100

    fixed majority_vote, also changed __ensemble_method__ to return the class instead of probabilities

--- src/TrainProdecure.py
+++ src/TrainProdecure.py
@@ -17,6 +17,21 @@ from LoadData import get_all_datasets_test_train_np_arrays
 from ModelBuilder import get_model_name
 
 
+def get_csv_name(used_model_names, num_datasets):
+    """
+    Returns a csv file name based on models and datasets
+
+    :param used_model_names: list of model names
+    :param num_datasets: number of used datasets
+    :return: csv file name
+    """
+    def model_list_to_str(models: List[Callable]):
+        return reduce(lambda result, m: result + '_' + m, models, "")
+    return ''.join([str(get_project_root() / "results"), "/train_",
+                    model_list_to_str(used_model_names), "_",
+                    str(num_datasets), "_datasets.csv"])
+
+
 def train_single_model(model: tf.keras.Model, x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray,
                        y_test: np.ndarray, epochs: int, learning_rate=None, batch_size: int | None = 25,
                        validation_split: float = 0.1, model_name: str = 'Unnamed model',
@@ -49,6 +64,7 @@ def train(model_builders: List[Callable],
           learning_rate=None):
     """
     Trains given models on given datasets
+
     :param model_builders: list of untrained models
     :param datasets: dict of datasets to be trained on
     :param epochs
@@ -60,12 +76,7 @@ def train(model_builders: List[Callable],
     """
 
     # csv creation
-    def model_list_to_str(models: List[Callable]):
-        return reduce(lambda result, m: result + '_' + get_model_name(m), models, "")
-
-    result_csv_path = ''.join([str(get_project_root() / "results"), "/train_",
-                               model_list_to_str(model_builders), "_",
-                               str(len(datasets)), "_datasets.csv"])
+    result_csv_path = get_csv_name(list(map(get_model_name, model_builders)), len(datasets))
     if not (csv_path := Path(result_csv_path)).exists():
         csv_path.touch()
     if len(csv_path.read_text()) == 0:
